35769817cc0c0170ed47d6811a1fad88109afa3e
paperbenni <paperbenni@gmail.com>
2021-05-17 17:44:15 +0200

    fix bugs

--- labor_loesung/src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
+++ labor_loesung/src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
@@ -3,7 +3,6 @@
  * @author wejo1037
  */
 
-
 package de.hska.iwi.ads.solution.list;
 
 import de.hska.iwi.ads.dictionary.AbstractDoubleLinkedList;
@@ -11,40 +10,58 @@ import java.util.AbstractMap.SimpleEntry;
 
 public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDoubleLinkedList<K, V> {
 
-	
-	public V get(Object o) {
+    public V get(Object o) {
         @SuppressWarnings("unchecked")
-        K searchkey = (K)o;
+        K searchkey = (K) o;
         return find(searchkey).entry.getValue();
-	}
+    }
 
     private ListElement find(K key) {
         ListElement a = null;
         ListElement counter = this.head;
-        while (counter.next != null) {
+        while (true) {
             if (counter.entry.getKey().compareTo(key) == 0) {
                 a = counter;
                 break;
             }
-            counter = counter.next;
+            if (counter.next == null) {
+                break;
+            } else {
+                counter = counter.next;
+            }
         }
-		return a;
+        return a;
     }
-	
-	public V put(K key, V value) {
-		V a = null;
+
+    public V put(K key, V value) {
+        if (this.head == null) {
+            SimpleEntry<K, V> newentry = new SimpleEntry<>(key, value);
+            ListElement newelement = new ListElement(newentry, null, this.head);
+            this.head = newelement;
+            return null;
+        }
+        V a = null;
         ListElement checker = find(key);
         if (checker != null) {
             a = checker.entry.getValue();
             checker.entry.setValue(value);
-        }  else {
+        } else {
             SimpleEntry<K, V> newentry = new SimpleEntry<>(key, value);
             ListElement newelement = new ListElement(newentry, null, this.head);
+            newelement.next = this.head;
             this.head.previous = newelement;
             this.head = newelement;
         }
         return a;
-	}
+    }
+
+    // public static void main(String[] args) {
+    //     DoubleLinkedList<Integer, String> testlist = new DoubleLinkedList<>();
+    //     testlist.put(8, "hello world");
+    //     testlist.put(9, "neun");
+    //     testlist.put(2, "zweiu");
+    //     testlist.put(1, "eins");
+    //     testlist.put(4, "view");
+    //     System.out.println(testlist.get(8));
+    // }
 }
-
-

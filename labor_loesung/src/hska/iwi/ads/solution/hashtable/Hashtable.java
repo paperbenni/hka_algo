c07f57d9c5c34f56253bdb703172b7b779c8baec
Baltimon <johannes.beni@gmail.com>
2021-05-27 18:58:47 +0200

    Hashtable.java erstellt und angefangen aber an Quadratischem sondieren gescheitert

---
+++ labor_loesung/src/hska/iwi/ads/solution/hashtable/Hashtable.java
@@ -0,0 +1,62 @@
+package hska.iwi.ads.solution.hashtable;
+
+import de.hska.iwi.ads.dictionary.AbstractHashMap;
+
+public class Hashtable<K extends Comparable<K>,V> extends AbstractHashMap<K, V> {
+
+	public Hashtable(int capacity) {
+		super(capacity);
+		// TODO Auto-generated constructor stub
+	}
+	public V get(Object o) {
+		K key = (K)o;
+		int keyHash = key.hashCode();
+		V ret = hashtable[keyHash].getValue();
+		if(ret==null) {
+			return null;
+		}
+		return ret;
+	}
+	
+	
+	
+	public V put(K key, V value) {
+		
+		
+		int keyHash = key.hashCode();
+		
+		if (hashtable[keyHash].getValue()==null) {
+		SimpleEntry<K,V> newEntry =  new SimpleEntry<>(key,value);
+		hashtable[keyHash] = newEntry;
+		}
+		else {
+			keyHash = quadratischSondieren(keyHash);
+			SimpleEntry<K,V> newEntry =  new SimpleEntry<>(key,value);
+			hashtable[keyHash] = newEntry;
+			
+		}
+		
+	}
+	public void delete(Object o) {
+		throw new UnsupportedOperationException("Dat Bot ist voll!");
+	}
+	
+	
+	
+	
+	private int quadratischSondieren(int hash){
+		hash=hash*hash;
+		if (hash<hashtable.length) {
+			throw new DictionaryFullException("Dat Bot ist voll!");
+		}
+		
+		if (hashtable[hash].getValue()==null) {
+			return hash;
+			}
+			else {
+				return quadratischSondieren(hash);
+			}
+	}
+	
+
+}

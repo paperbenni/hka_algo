97411c518a4c39d302da78871fdfbbe3d1d748bc
Baltimon <johannes.beni@gmail.com>
2021-05-28 10:43:27 +0200

    Hashtable.java fertig !! ... ? tests Fehlen noch

--- labor_loesung/src/hska/iwi/ads/solution/hashtable/Hashtable.java
+++ labor_loesung/src/hska/iwi/ads/solution/hashtable/Hashtable.java
@@ -9,61 +9,65 @@ public class Hashtable<K extends Comparable<K>,V> extends AbstractHashMap<K, V> 
 		// TODO Auto-generated constructor stub
 	}
 	public V get(Object o) {
-		K key = (K)o;
-		V ret;
-		int keyHash = key.hashCode();
-		while(hashtable[keyHash].getKey()!=key) {
-			keyHash=keyHash*keyHash%hashtable.length;
-		}
-
-			ret = hashtable[keyHash].getValue();
-			
-		if(ret==null) {
+		K key = (K)o; 						// key wird zu K gecastet
+		int i =0;
+		int keyHash = Math.abs(key.hashCode()%hashtable.length);// hash berechnen
+		if (hashtable[keyHash]==null) {
 			return null;
 		}
-		return ret;
+		while(hashtable[keyHash].getKey()!=key&& i<hashtable.length) {
+			i++;
+			keyHash=quadtatischSondiere(keyHash);
+		}
+		if(i>=hashtable.length) {
+			return null;
+		}
+			return hashtable[keyHash].getValue();
+
 	}
 	
-	
-	
-	
+
 	public V put(K key, V value) {
 		
-		
-		int keyHash = key.hashCode();
-		
-		if (hashtable[keyHash].getValue()==null) {
+		int keyHash = Math.abs(key.hashCode()%hashtable.length);
+		int i =0;
+		V ret = value;
+		if (hashtable[keyHash]==null||hashtable[keyHash].getKey()==null) {
 		SimpleEntry<K,V> newEntry =  new SimpleEntry<>(key,value);
 		hashtable[keyHash] = newEntry;
 		}
 		else {
-			keyHash = quadratischSondieren(keyHash);
+			while(hashtable[keyHash].getKey()!=key&& i<hashtable.length) {
+				i++;
+				keyHash=quadtatischSondiere(keyHash);
+			}
+
 			SimpleEntry<K,V> newEntry =  new SimpleEntry<>(key,value);
+			ret = hashtable[keyHash].getValue();
 			hashtable[keyHash] = newEntry;
-			
 		}
+		return ret;
 		
 	}
+	private int quadtatischSondiere(int hash) {
+		return Math.abs((hash*hash)%hashtable.length);
+	}
+	
 	public void delete(Object o) {
-		throw new UnsupportedOperationException("Dat Bot ist voll!");
+		throw new UnsupportedOperationException("Was mein ist bleibt auch mein !");
 	}
 	
 	
-	
-	
-	private int quadratischSondieren(int hash){
-		hash=hash*hash;
-		if (hash<hashtable.length) {
-			throw new DictionaryFullException("Dat Bot ist voll!");
-		}
+	public static void main(String[] args) {
+		Hashtable<Integer, String> Map = new Hashtable<>(4);
+		Map.put(1, "Gärster");
+		Map.put(1, "Müller");
+		Map.put(123, "Riese");
 		
-		if (hashtable[hash].getValue()==null) {
-			return hash;
-			}
-			else {
-				return quadratischSondieren(hash);
-			}
+		System.out.println(Map.get(1));
+		System.out.println(Map.get(123));
+		
+		
 	}
-	
 
 }
